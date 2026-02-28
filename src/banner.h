db2effeddb3f21faf49d3976eb1ec41acd3b6bc8
temporal-architect <amexsimoes@gmail.com>
2026-03-01 05:42:30 +0800

    fix: restore Singapore Zenith identity and NS-33 build stability

---
+++ src/banner.h
@@ -0,0 +1,9 @@
+#ifndef _banner_h_INCLUDED
+#define _banner_h_INCLUDED
+
+#define LIGHT_BLUE  "\033[1;34m"
+#define NORMAL      "\033[0m"
+
+void kissat_banner (const char * name, const char * description);
+
+#endif
